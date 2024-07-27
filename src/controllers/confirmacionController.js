5b13520f1450440b3e3b6cdbdf85cf9549ee5ac3
SergioCarr <carreon.sergioivan@gmail.com>
2024-07-26 20:37:29 -0600

    agregacion de confirmacionPedido mejorada

--- src/controllers/confirmacionController.js
+++ src/controllers/confirmacionController.js
@@ -29,8 +29,7 @@ router.get('/pedidosEnCurso',async (req,res) => {
     console.log('inicia el query')
     const userInfo = await db.query("find","clientes",{_id:db.objectID(req.session.userID)},{nombre:1,telefono:1,_id:0})
     const estados = ["Esperando confirmacion","En proceso","Listo para recoger","Cancelado"]
-    const pedidoInfo = await db.query("aggregation","pedidos",[{$match:{cliente: req.session.userMail,estado:{$in:estados}}},{$sort:{entrega:-1}},{$limit:1},{$project:{_id:1,especificaciones:1,descripcion:1, entrega:1,estado:1,clave:1,pickup:1}}])
-    const pedidoInfos = await db.query("find","pedidos",{cliente: req.session.userMail,estado:{$in:estados}},{_id:1,especificaciones:1,descripcion:1, entrega:1,estado:1,clave:1,pickup:1})
+    const pedidoInfo = await db.query("aggregation","pedidos",[{$match:{cliente: req.session.userMail,estado:{$in:estados}}},{$sort:{entrega:-1}},{$limit:1},{$lookup:{from:"proveedores",localField:"proveedor",foreignField:"correo",as:"infoProveedor"}},{$project:{_id:1,especificaciones:1,descripcion:1, entrega:1,estado:1,clave:1,pickup:1,nombre:"$infoProveedor.nombre",telefono:"$infoProveedor.telefono"}},{$unwind:"$nombre"},{$unwind:"$telefono"}])
     let resultado = []
     pedidoInfo.forEach(pedido=>{
         let total = 0
@@ -44,15 +43,15 @@ router.get('/pedidosEnCurso',async (req,res) => {
         resultado.push(
             {
                 id:id.substring(id.length-8,id.length-2).toUpperCase(),
-                nombre: userInfo[0].nombre,
-                telefono: userInfo[0].telefono,
+                nombre: pedido.nombre,
+                telefono: pedido.telefono,
                 especificaciones: pedido.especificaciones,
                 total: total,
                 descripcion: descripcion,
                 entrega: pedido.entrega.toLocaleString(),
                 status: pedido.estado,
                 clave: pedido.clave,
-                pickup: pedido.pickup
+                pickup: pedido.pickup=="mostrador"?"Mostrador":"Foodie-box"
             }
         )
     })
