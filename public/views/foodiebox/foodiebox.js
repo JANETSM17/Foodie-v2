ed450d2b16f713eefcbf678bbcc90ceb3a4a63fa
SergioCarr <carreon.sergioivan@gmail.com>
2024-07-27 15:03:00 -0600

    push x

---
+++ public/views/foodiebox/foodiebox.js
@@ -0,0 +1,12 @@
+function irHome() {
+    window.location.href = "/homeP";
+  };
+function irQA() {
+    window.location.href = "/providerQA";
+  };
+function irContact() {
+    window.location.href = "/contactUsE";
+  };
+function irProfile() {
+    window.location.href = "/providerProfile";
+  }
